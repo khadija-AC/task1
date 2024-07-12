c867c8e6e6a16b5e287be4943016be1c3e6c7817
admin <khadijaaws21@gmail.com>
2024-07-12 19:07:45 +0200

    my application

---
+++ app.js
@@ -0,0 +1,14 @@
+var a= require('a.js')
+
+let array = ['bonjour' . "khadija" , 'cv', 2.57 
+]
+
+for(let i = 0; i < array.length; i++) {
+    var element = array[i]
+}
+
+function direBonjour( monnom ) {
+     var age=2
+}
+
+const age = 12 
