171906710e45a726e7f89f352e7bd8b6bb98e096
khadija-AC <khadijaaws21@gmail.com>
2024-07-13 15:29:05 +0100

    ajoute erreur

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var a = require("a.js");
+var a = require("a.js")
 
-let array = ["bonjour", "khadija", "cv", 2.57];
+let array = ['bonjour', "khadija", "cv", 2.57];
 
 for (let i = 0; i < array.length; i++) {
-  var element = array[i];
+ 
+
+
+
+
+
+ var element = array[i];
 }
 
 function direBonjour(monnom) {
